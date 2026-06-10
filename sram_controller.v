1d866984da49cae2b6bdf40df0bff12fb066839f
fat-file-error <Fat-File-error@users.noreply.github.com>
2026-06-10 22:55:06 +0530

    Add SRAM controller FSM architecture

--- sram_controller.v
+++ sram_controller.v
@@ -31,3 +31,47 @@ wire [7:0] rdata_from_sram;
     .wdata(wdata_to_sram),
     .rdata(rdata_from_sram)
 );
+parameter IDLE  = 2'b00;
+    parameter READ  = 2'b01;
+    parameter WRITE = 2'b10;
+    parameter DONE  = 2'b11;
+
+    reg [1:0] state;
+    reg [1:0] next_state;
+  
+  	always @(posedge clk or posedge rst) begin
+    if (rst)
+        state <= IDLE;
+    else
+        state <= next_state;
+	end
+  
+  always @(*) begin
+    case(state)
+      
+      IDLE : 
+        begin
+          if(wr_req)
+            next_state = WRITE ; 
+          
+          else if(rd_req)
+            next_state = READ ;
+          
+          else
+            next_state = IDLE ;
+        end
+      
+      READ :
+       	next_state = DONE;
+      
+      WRITE :
+        next_state = DONE;
+      
+      DONE:
+        next_state = IDLE;
+      
+      default:
+        next_state = IDLE;
+    endcase
+          
+  end
